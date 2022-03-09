ea83784a917cad97f4ba72cff47ff61f85bbb5b6
DavidHermi <dhermidaperez@danielcastelao.org>
2022-03-09 14:32:47 +0100

    commit  de pedir token fichero funciona

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,29 @@
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
+        GitHub github = null ;
 
+        try {
+            github = GitHubBuilder.fromPropertyFile("/home/dam1/Escritorio/token.properties").build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            GHRepository repo = github.createRepository(
+                    "new-repository3" ).create() ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
     }
 
 
