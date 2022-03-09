1d48e2b771107fc60697ab689f6ea1742693ca3b
DavidHermi <dhermidaperez@danielcastelao.org>
2022-03-09 14:27:07 +0100

    commit  de pedir token funciona

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,32 @@
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+
+import javax.swing.*;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
+        GitHub github = null ;
 
+        try {
+            github = new GitHubBuilder().withOAuthToken(JOptionPane.showInputDialog("Inserte el token")).build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            GHRepository repo = github.createRepository(
+                    "new-repository2" ).create() ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
     }
 
 
-}
+
